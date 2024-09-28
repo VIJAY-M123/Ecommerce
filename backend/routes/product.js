0814706f3fd0b6f96e10af2c0f21cf9f57e048c2
Vijaya Raj <vijayaraj@aggrandizeventure.com>
2024-09-28 10:21:55 +0530

    Second Commit

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getProducts, newProduct, getSingleProduct, updateProdcut, deleteProdcut } = require('../controllers/productController');
 const router = express.Router();
+const {isAuthenticate, autherizationRole} = require('../middlewares/authenticate')
 
-router.route('/products').get(getProducts);
-router.route('/products/new').post(newProduct);
+router.route('/products').get(isAuthenticate, getProducts);
+router.route('/products/new').post(isAuthenticate,autherizationRole('admin'),newProduct);
 router.route('/products/:id').get(getSingleProduct);
 router.route('/products/:id').put(updateProdcut);
 router.route('/products/:id').delete(deleteProdcut);
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,27 @@ const connectDataBase = require('./config/database');
 dotenv.config({path:path.join(__dirname,"config/config.env")}) //absolute path
 connectDataBase();
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
 console.log(`Server listening port ${process.env.PORT} in ${process.env.NODE_ENV}`);
 })
+
+process.on('unhandledRejection',(err)=>{
+console.log(`Error: ${err.message}`);
+console.log("shutting down unhandledRejection ");
+server.close(()=>{
+    process.exit(1);
+})
+})
+
+process.on('uncaughtException',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("uncaught Exception error ");
+    server.close(()=>{
+        process.exit(1);  
+    })
+})
+
+
+
+
+
